2b54251588d4c2e8101e2438144cf29fb89b13dc
benamonya <benanyanga@gmail.com>
2024-09-25 11:37:08 +0300

    Copyright and cleanup

--- app/src/main/java/com/muzima/utils/ThemeUtils.java
+++ app/src/main/java/com/muzima/utils/ThemeUtils.java
@@ -13,7 +13,6 @@ package com.muzima.utils;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.graphics.drawable.Drawable;
 
 import com.muzima.R;
